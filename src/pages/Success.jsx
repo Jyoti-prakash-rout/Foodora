d99471740e1a33f842fddec89ad173fd7205d360
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2026-01-04 15:55:09 +0530

    Add clearCart action and clear cart on order success
    Introduces a clearCart reducer to CartSlice and dispatches it in the Success page to empty the cart after a successful order. Also enhances the Success page UI with order ID and a link to return home.

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { PulseLoader } from "react-spinners";
+import { useDispatch } from "react-redux";
+
+import { clearCart } from "../redux/slices/CartSlice";
+import { Link } from "react-router-dom";
+
+import { ArrowLeft } from "lucide-react";
 
 const Success = () => {
+  const dispatch = useDispatch();
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -9,6 +17,10 @@ const Success = () => {
       setLoading(false);
     }, 2000);
   }, []);
+
+  useEffect(() => {
+    dispatch(clearCart());
+  }, [dispatch]);
 
   return (
     <>
@@ -18,13 +30,22 @@ const Success = () => {
         ) : (
           <div>
             <h2 className="text-3xl font-semibold mb-4 text-center">
-              Order Successful!
+              Order Successful 🎉
             </h2>
             <p>Your order has been successfully placed</p>
+            <p>Order ID: #FOOD{Date.now()}</p>
+
+            <div className="flex justify-center">
+              <Link
+                to="/"
+                className="text-green-500 font-bold mt-10 text-center">
+                Back to home 
+              </Link>
+            </div>
           </div>
         )}
       </div>
-    </> 
+    </>
   );
 };
 
